147aafc03c9b1c19f7902a0789b27461793b61a5
Blake Ramsdell <blake@synchro.io>
2014-12-24 00:21:15 -0500

    Added SplashActivity.

---
+++ app/src/main/java/io/synchro/client/android/SplashActivity.java
@@ -0,0 +1,13 @@
+package io.synchro.client.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
