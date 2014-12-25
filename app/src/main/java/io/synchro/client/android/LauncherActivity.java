67b76590284ea912c0d593a18fffedf3cad2ea2f
Blake Ramsdell <blake@synchro.io>
2014-12-25 00:14:10 -0500

    Added context menu on long press. I enjoyed it.

--- app/src/main/java/io/synchro/client/android/LauncherActivity.java
+++ app/src/main/java/io/synchro/client/android/LauncherActivity.java
@@ -1,21 +1,87 @@
 package io.synchro.client.android;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
-public class LauncherActivity extends ActionBarActivity
+public class LauncherActivity extends Activity
 {
+    private ActionMode actionMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-        ((ListView) findViewById(R.id.appListView)).setAdapter(new SynchroAppTwoLineArrayAdapter(this, AndroidSynchroAppManager.getAppManager(this).getApps()));
+        ListView appListView = (ListView) findViewById(R.id.appListView);
+        appListView.setAdapter(
+                new SynchroAppTwoLineArrayAdapter(
+                        this, AndroidSynchroAppManager.getAppManager(this).getApps()
+                )
+                              );
+        appListView.setLongClickable(true);
+        appListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+                                           {
+                                               @Override
+                                               public boolean onItemLongClick(
+                                                       AdapterView<?> parent, View view,
+                                                       int position, long id)
+                                               {
+                                                   if (actionMode != null)
+                                                   {
+                                                       return false;
+                                                   }
+
+                                                   actionMode = startActionMode(new ActionMode.Callback()
+                                                                                {
+                                                                                    @Override
+                                                                                    public boolean onCreateActionMode(
+                                                                                            ActionMode mode,
+                                                                                            Menu menu
+                                                                                                                     )
+                                                                                    {
+                                                                                        MenuInflater inflater = mode.getMenuInflater();
+                                                                                        inflater.inflate(R.menu.menu_app_context, menu);
+                                                                                        return true;
+                                                                                    }
+
+                                                                                    @Override
+                                                                                    public boolean onPrepareActionMode(
+                                                                                            ActionMode mode,
+                                                                                            Menu menu
+                                                                                                                      )
+                                                                                    {
+                                                                                        return false;
+                                                                                    }
+
+                                                                                    @Override
+                                                                                    public boolean onActionItemClicked(
+                                                                                            ActionMode mode,
+                                                                                            MenuItem item
+                                                                                                                      )
+                                                                                    {
+                                                                                        return false;
+                                                                                    }
+
+                                                                                    @Override
+                                                                                    public void onDestroyActionMode(
+                                                                                            ActionMode mode
+                                                                                                                   )
+                                                                                    {
+                                                                                        actionMode = null;
+                                                                                    }
+                                                                                });
+                                                   view.setSelected(true);
+                                                   return true;
+                                               }
+                                           });
     }
 
 
