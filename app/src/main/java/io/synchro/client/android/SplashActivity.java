3e6b2d76490327121e587f356eab7fde470e61c2
Blake Ramsdell <blake@synchro.io>
2014-12-24 22:23:55 -0500

    Added timer to keep splash around.

--- app/src/main/java/io/synchro/client/android/SplashActivity.java
+++ app/src/main/java/io/synchro/client/android/SplashActivity.java
@@ -3,6 +3,7 @@ package io.synchro.client.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import java.io.IOException;
@@ -10,23 +11,34 @@ import java.io.IOException;
 public class SplashActivity extends Activity
 {
     public static final String TAG = SplashActivity.class.getSimpleName();
+    private static final int SPLASH_TIME_OUT_MS = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        AndroidSynchroAppManager appManager = AndroidSynchroAppManager.getAppManager(this);
-        try
-        {
-            appManager.load();
-        }
-        catch (IOException e)
-        {
-            Log.wtf(TAG, e);
-        }
+        // http://www.androidhive.info/2013/07/how-to-implement-android-splash-screen-2/
 
-        startActivity(new Intent(this, LauncherActivity.class));
-        finish();
+        new Handler().postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                  // This method will be executed once the timer is over
+                  // Start your app main activity
+
+                  AndroidSynchroAppManager appManager = AndroidSynchroAppManager.getAppManager(SplashActivity.this);
+                  try
+                  {
+                      appManager.load();
+                  }
+                  catch (IOException e)
+                  {
+                      Log.wtf(TAG, e);
+                  }
+
+                  startActivity(new Intent(SplashActivity.this, LauncherActivity.class));
+                  finish();
+              }
+          }, SPLASH_TIME_OUT_MS);
     }
 }
