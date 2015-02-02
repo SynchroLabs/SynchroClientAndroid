da566faab815a3501ae0bdb7c41ca1030259503a
Blake Ramsdell <blake@synchro.io>
2015-02-02 15:47:52 -0800

    Added rebind property value test.

--- app/src/androidTest/java/io/synchro/client/android/PropertyValueTest.java
+++ app/src/androidTest/java/io/synchro/client/android/PropertyValueTest.java
@@ -42,4 +42,50 @@ public class PropertyValueTest extends TestCase
 
         assertEquals("The Colors are Red, Green, and Blue", propVal.Expand().asString());
     }
+
+    public void testPropertyValueModelUpdate()
+            throws IOException
+    {
+        JObject viewModel = (JObject) JObject.parse(
+                "{" +
+                        "\"serial\" : 0," +
+                        "\"title\" : \"Colors\"," +
+                        "\"colors\" :" +
+                        "[" +
+                        "{" +
+                        "\"name\" : \"Red\"," +
+                        "\"color\" : \"red\"," +
+                        "\"value\" : \"0xff0000\"" +
+                        "}," +
+                        "{" +
+                        "\"name\" : \"Green\"," +
+                        "\"color\" : \"green\"," +
+                        "\"value\" : \"0x00ff00\"" +
+                        "}," +
+                        "{" +
+                        "\"name\" : \"Blue\"," +
+                        "\"color\" : \"blue\"," +
+                        "\"value\" : \"0x0000ff\"" +
+                        "}," +
+                        "]" +
+                        "}");
+
+        BindingContext bindingCtx = new BindingContext(viewModel);
+
+        PropertyValue propVal = new PropertyValue("The {title} are {colors[0].name}, {colors[1].name}, and {colors[2].name}", bindingCtx);
+
+        assertEquals("The Colors are Red, Green, and Blue", propVal.Expand().asString());
+
+        JObject newColor = new JObject();
+        newColor.put("name", new JValue("Greenish"));
+        newColor.put("color", new JValue("green"));
+        newColor.put("value", new JValue("0x00ff00"));
+        ((JArray)viewModel.get("colors")).set(1, newColor);
+        for (BindingContext bindingContext : propVal.getBindingContexts())
+        {
+            bindingContext.Rebind();
+        }
+
+        assertEquals("The Colors are Red, Greenish, and Blue", propVal.Expand().asString());
+    }
 }
