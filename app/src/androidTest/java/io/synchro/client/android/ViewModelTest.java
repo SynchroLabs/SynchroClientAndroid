4dd34672355428ae47ef62b3b336ba77560b9c55
Blake Ramsdell <blake@synchro.io>
2015-02-14 14:51:57 -0800

    Added testUpdateViewFromValueBinding.

--- app/src/androidTest/java/io/synchro/client/android/ViewModelTest.java
+++ app/src/androidTest/java/io/synchro/client/android/ViewModelTest.java
@@ -3,6 +3,7 @@ package io.synchro.client.android;
 import junit.framework.TestCase;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by blake on 2/14/15.
@@ -87,5 +88,99 @@ public class ViewModelTest extends TestCase
 
         assertEquals("Serial: 1", serialString[0]);
         assertEquals(1, serialValue[0]);
+    }
+
+    public void testUpdateViewFromValueBinding()
+    {
+        ViewModel viewModel = new ViewModel();
+
+        viewModel.InitializeViewModelData(viewModelObj);
+
+        final boolean[] bindingsInitialized = new boolean[] { false };
+
+        final String[] serialString = new String[] { "" };
+        PropertyBinding propBinding = viewModel.CreateAndRegisterPropertyBinding(
+                viewModel.getRootBindingContext(),
+                "Serial: {serial}",
+                new ISetViewValue()
+                {
+                    @Override
+                    public void SetViewValue(JToken value)
+                    {
+                        serialString[0] = value.asString();
+                    }
+                }
+            );
+
+        final String[] titleString = new String[] { "" };
+        PropertyBinding propBindingTitle = viewModel.CreateAndRegisterPropertyBinding(
+                viewModel.getRootBindingContext(),
+                "Title: {title}",
+                new ISetViewValue()
+                {
+                    @Override
+                    public void SetViewValue(JToken value)
+                    {
+                        titleString[0] = value.asString();
+                        if (bindingsInitialized[0])
+                        {
+                            fail("Property binding setter for title should not be called after initialization (since its token wasn't impacted by the value binding change)");
+                        }
+                    }
+                }
+             );
+
+        final int[] serialValue = new int[] { -1 };
+        ValueBinding valBinding = viewModel.CreateAndRegisterValueBinding(
+                viewModel.getRootBindingContext().Select("serial"),
+                new IGetViewValue()
+                {
+                    @Override
+                    public JToken GetViewValue()
+                    {
+                        return new JValue(serialValue[0]);
+                    }
+                },
+                new ISetViewValue()
+                {
+                    @Override
+                    public void SetViewValue(JToken value)
+                    {
+                        serialValue[0] = value.asInt();
+                        if (bindingsInitialized[0])
+                        {
+                            fail("Value bining setter should not be called after initialization (its change shouldn't update itself)");
+                        }
+                    }
+                }
+            );
+
+        propBinding.UpdateViewFromViewModel();
+        propBindingTitle.UpdateViewFromViewModel();
+        valBinding.UpdateViewFromViewModel();
+
+        bindingsInitialized[0] = true;
+
+        assertEquals("Serial: 1", serialString[0]);
+        assertEquals("Title: Colors", titleString[0]);
+        assertEquals(1, serialValue[0]);
+
+        // When the value binding updates the view model, the propBinding (that has a token bound to the same context/path) will automatically
+        // update (its setter will be called), but the value binding that triggered the update will not have its setter called.
+        //
+        serialValue[0] = 2;
+        valBinding.UpdateViewModelFromView();
+
+        assertEquals("Serial: 2", serialString[0]);
+
+        // Now let's go collect the changes caused by value binding updates and verify them...
+        //
+        Map<String, JToken> changes = viewModel.CollectChangedValues();
+        assertEquals(1, changes.size());
+        assertEquals(2, changes.get("serial").asInt());
+
+        // Collecting the changes (above) should have cleared the dirty indicators, so there shouldn't be any changes now...
+        //
+        assertEquals(0, viewModel.CollectChangedValues().size());
     }
 }
