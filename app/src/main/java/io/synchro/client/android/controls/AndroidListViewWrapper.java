bdbf379d5e88497f25554cd0476272d190cea725
Robert Dickinson <bob.dickinson@gmail.com>
2016-05-18 12:44:19 -0700

    Test for WRAP_CONTENT in onMeasure fix.

--- app/src/main/java/io/synchro/client/android/controls/AndroidListViewWrapper.java
+++ app/src/main/java/io/synchro/client/android/controls/AndroidListViewWrapper.java
@@ -252,9 +252,10 @@ public class AndroidListViewWrapper extends AndroidControlWrapper
 
         @Override
         public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
-                    MeasureSpec.AT_MOST);
-            super.onMeasure(widthMeasureSpec, expandSpec);
+            if (this.getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+                heightMeasureSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+            }
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
     }
 
